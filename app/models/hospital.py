5c704103107a7f29a454ef8017992dd5ddf3f94f
MrShokh4346 <shokhruxabdurasulov0@gmail.com>
2026-01-27 17:00:25 +0500

    Update hospital.py

--- app/models/hospital.py
+++ app/models/hospital.py
@@ -403,7 +403,11 @@ class RemainderSumFromReservation(Base):
     @classmethod
     async def set_remainder(cls, db: AsyncSession, **kwargs):
         try:
-            obj = cls(**kwargs)
+            obj = cls(
+                        amonut=kwargs['amonut'],
+                        pharmacy_id=kwargs['pharmacy_id'],
+                        reservation_invoice_number=kwargs['reservation_invoice_number']
+                    )
             db.add(obj)
         except IntegrityError as e:
             raise HTTPException(status_code=404, detail=str(e.orig).split('DETAIL:  ')[1].replace('.\n', ''))
